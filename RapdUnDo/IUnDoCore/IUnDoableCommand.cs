2d562394adc676c65fe56dadc4a1e3f62c501571
SigurdJanson <emailjs@web.de>
2021-07-24 17:10:11 +0200

    Interface for undo commands

---
+++ RapdUnDo/IUnDoCore/IUnDoableCommand.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace RapdUnDo.IUndoCore
+{
+    interface IUnDoableCommand
+    {
+        public void Execute();
+
+        public void Revert();
+
+        //public RenderFragment DisplayCommand();
+    }
+}
