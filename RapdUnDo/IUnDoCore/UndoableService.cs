2d34aa62f61758d9120eab0af556e35a6966b586
SigurdJanson <emailjs@web.de>
2021-07-25 22:54:46 +0200

    Draft implementation of the undoable service

---
+++ RapdUnDo/IUnDoCore/UndoableService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MudBlazor;
+
+
+namespace RapdUnDo.IUndoCore
+{
+    public class UndoableService : IUndoableService
+    {
+        protected ISnackbar SnackbarService { get; set; }
+
+        protected HashSet<IUndoableCommand> Commands { get; set; }
+
+        public int GracePeriod { get; set; }
+
+        public UndoableService(ISnackbar snackbar, int gracePeriod = 5000)
+        {
+            SnackbarService = snackbar;
+            GracePeriod = gracePeriod;
+        }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+            GC.SuppressFinalize(this);
+        }
+
+
+        public void Register(IUndoableCommand command)
+        {
+            Commands.Add(command);
+            throw new NotImplementedException(); // register to fire event
+        }
+
+        public void RegisterList(IEnumerable<IUndoableCommand> commands)
+        {
+            foreach (var c in commands)
+                Register(c);
+        }
+
+
+        protected void Show()
+        {
+            SnackbarService.Add("Ooops. Something really bad happened!", Severity.Normal, config =>
+            {
+                config.Action = "Help";
+                config.ActionColor = Color.Primary;
+                config.Onclick = snackbar =>
+                {
+                    Help();
+                    return Task.CompletedTask;
+                };
+            });
+        }
+    }
+}
