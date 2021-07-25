00f3c02549223bf9a6b9ee38d7b0b59e8a1e0bd8
SigurdJanson <emailjs@web.de>
2021-07-25 22:52:39 +0200

    Added interface for undo service

---
+++ RapdUnDo/IUnDoCore/IUndoableService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MudBlazor;
+
+namespace RapdUnDo.IUndoCore
+{
+    public interface IUndoableService : IDisposable
+    {
+        /// <summary>
+        /// The time period a UI widget is visible and allows users to undo an action.
+        /// </summary>
+        public int GracePeriod { get; set; }
+
+        public void Register(IUndoableCommand command);
+
+        public void RegisterList(IEnumerable<IUndoableCommand> commands);
+
+        public void OnExecuted(IUndoableCommand command);
+    }
+}
