9d8bda67f81256232248405dbe1be6733d10170c
SigurdJanson <emailjs@web.de>
2021-07-27 21:36:53 +0200

    Register undo service and use it in counter page

--- RapdUnDo/Program.cs
+++ RapdUnDo/Program.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using MudBlazor.Services;
+using RapdUnDo.IUndoCore;
 
 namespace RapdUnDo
 {
@@ -20,6 +21,7 @@ namespace RapdUnDo
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMudServices();
+            builder.Services.AddScoped<IUndoableService, UndoableService>();
 
             await builder.Build().RunAsync();
         }
